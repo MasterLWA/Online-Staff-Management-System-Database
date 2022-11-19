a3741038d43c1b01fcec4cca78de78cdab2f1a7d
Lakindu Widuranga Alwis <29312088+MasterLWA@users.noreply.github.com>
2022-11-19 07:31:40 +0530

    Update Online Staff management System Database.sql

--- Online Staff management System Database.sql
+++ Online Staff management System Database.sql
@@ -1,17 +1,19 @@
-SELECT `staff`.`staffID`,
-    `staff`.`password`,
-    `staff`.`firstName`,
-    `staff`.`lastName`,
-    `staff`.`p_email`,
-    `staff`.`w_email`,
-    `staff`.`profile_pic`,
-    `staff`.`birthday`,
-    `staff`.`gender`,
-    `staff`.`phoneNumber`,
-    `staff`.`address`,
-    `staff`.`hireDate`,
-    `staff`.`quitDate`,
-    `staff`.`salary`,
-    `staff`.`ot_rate`,
-    `staff`.`staffcol`
-FROM `osms`.`staff`;
+CREATE TABLE `admin` (
+  `adminID` int NOT NULL AUTO_INCREMENT,
+  `admin_username` varchar(45) NOT NULL,
+  `admin_password` varchar(45) NOT NULL,
+  PRIMARY KEY (`adminID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+CREATE TABLE `attendance` (
+  `attendance_id` int NOT NULL AUTO_INCREMENT,
+  `staff_id` int DEFAULT NULL,
+  `offTime` time NOT NULL,
+  `onTime` time NOT NULL,
+  `hours_worked` time NOT NULL,
+  PRIMARY KEY (`attendance_id`),
+  KEY `FK_attendance_staff_id` (`staff_id`),
+  CONSTRAINT `FK_attendance_staff_id` FOREIGN KEY (`staff_id`) REFERENCES `staff` (`staffID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+
